9acfba35f71fba201c851a11ceb7a6cad3971d99
Nikita <nikitakarpinghikus@gmail.com>
2025-04-27 16:35:38 +0300

    Create Spring Data JPA repositories

---
+++ src/main/java/com/tech/listify/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.tech.listify.repository;
+
+import com.tech.listify.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
---
+++ src/main/java/com/tech/listify/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.tech.listify.repository;
+
+import com.tech.listify.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String username);
+    boolean existsByEmail(String email);
+}
