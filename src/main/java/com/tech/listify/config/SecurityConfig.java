86efe77b9bee24fc9f37454561f43c31759b7ae1
Nikita <nikitakarpinghikus@gmail.com>
2025-07-04 22:34:12 +0300

    update CORS

--- src/main/java/com/tech/listify/config/SecurityConfig.java
+++ src/main/java/com/tech/listify/config/SecurityConfig.java
@@ -95,7 +95,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html").permitAll()
-                        .anyRequest().permitAll()
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
                 );
         http.addFilterAt(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
